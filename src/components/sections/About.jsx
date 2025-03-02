201b554ea009c8e0f969f9c2f26bc51d9d516562
HASSANE SKIKRI <hassaneskikri@gmail.com>
2025-03-02 18:02:21 +0000

    Refactor project structure: update favicon, adjust image paths, and remove unused components and utility functions

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -73,7 +73,7 @@ const AboutSection = () => {
             >
               <div className="aspect-square w-72 h-72 overflow-hidden rounded-full border-4 border-blue-100 dark:border-blue-900/30">
                 <img
-                  src="../assets/About/About_photo.png"
+                  src="../assets/images/About_photo.png"
                   alt="Hassane SKIKRI"
                   className="w-full h-full object-cover object-center"
                 />
